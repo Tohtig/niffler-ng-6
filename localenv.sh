89f562e4dfd6b5faf6dfa68733ba6454bd2918b4
aippolitov <aippolitov.fil-it@bankismart.com>
2024-09-12 21:13:41 +0300

    fix startup

--- localenv.sh
+++ localenv.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo '### Remove docker containers ###'
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker run --name niffler-all -p 5432:5432 -e POSTGRES_PASSWORD=secret -e CREATE_DATABASES=niffler-auth,niffler-currency,niffler-spend,niffler-userdata -v pgdata:/var/lib/postgresql/data -d postgres:15.1
+echo '### Run postgres ###'
+#docker run --name niffler-all -p 5432:5432 -e POSTGRES_PASSWORD=secret -e CREATE_DATABASES=niffler-auth,niffler-currency,niffler-spend,niffler-userdata -v pgdata:/var/lib/postgresql/data -v ./postgres/init-database.sh:/docker-entrypoint-initdb.d/init-database.sh -d postgres:15.1 --max_prepared_transactions=100
+docker run --name niffler-all -p 5432:5432 -e POSTGRES_PASSWORD=secret -v pgdata:/var/lib/postgresql/data -d postgres:15.1
 docker run --name=zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 -p 2181:2181 -d confluentinc/cp-zookeeper:7.3.2
 docker run --name=kafka -e KAFKA_BROKER_ID=1 \
 -e KAFKA_ZOOKEEPER_CONNECT=$(docker inspect zookeeper --format='{{ .NetworkSettings.IPAddress }}'):2181 \
@@ -12,3 +15,10 @@ docker run --name=kafka -e KAFKA_BROKER_ID=1 \
 -e KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1 \
 -e KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1 \
 -p 9092:9092 -d confluentinc/cp-kafka:7.3.2
+
+echo '### Run front ###'
+cd ./niffler-ng-client/
+npm i
+#npm run build:windows
+npm run build:dev
+cd ../
